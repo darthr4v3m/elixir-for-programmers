4330f2f4264e88a4d8409ddecb60db018ae0e2a5
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-24 21:49:30 +0200

    feat(game-logic): :sparkles: win/lose logic

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -23,4 +23,14 @@ defmodule HangmanImplGameTest do
 
     assert game.letters |> List.to_charlist() |> List.ascii_printable?()
   end
+
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do
+      game = "batman" |> Game.new_game()
+      game = Map.put(game, :game_state, :won)
+      {new_game, _tally} = Game.make_move(game, "x")
+
+      assert new_game == game
+    end
+  end
 end
