215749b2ab3a9e83aece7ce520915a9c57fa2d8c
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-09-25 22:24:55 +0200

    feat(game-logic): :sparkles: check for duplicate moves

--- hangman/hangman/test/impl_game_test.exs
+++ hangman/hangman/test/impl_game_test.exs
@@ -27,10 +27,28 @@ defmodule HangmanImplGameTest do
   test "state doesn't change if a game is won or lost" do
     for state <- [:won, :lost] do
       game = "batman" |> Game.new_game()
-      game = Map.put(game, :game_state, :won)
+      game = Map.put(game, :game_state, state)
       {new_game, _tally} = Game.make_move(game, "x")
 
       assert new_game == game
     end
+  end
+
+  test "a duplicate letter is reported" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "y")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "we record letters used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    {game, _tally} = Game.make_move(game, "y")
+    {game, _tally} = Game.make_move(game, "x")
+    assert MapSet.equal?(game.used, MapSet.new(["x", "y"]))
   end
 end
