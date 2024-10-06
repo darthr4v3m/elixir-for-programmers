2af11e0f97aee3fddc9c1c7d5f9d0cc5feeede04
darthr4v3m <3935459+darthr4v3m@users.noreply.github.com>
2024-10-06 22:19:35 +0200

    refactor(dictionary): :recycle: separate API and implementation

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,11 +1,4 @@
 defmodule Dictionary do
-
-  @word_list "assets/words.txt"
-  |> File.read!()
-  |> String.split(~r/\n/, trim: true)
-
-  def random_word do
-    @word_list
-    |> Enum.random()
-  end
+  defdelegate start(), to: Dictionary.Impl.WordList
+  defdelegate random_word(word_list), to: Dictionary.Impl.WordList
 end
